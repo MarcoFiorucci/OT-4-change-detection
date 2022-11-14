0a63ed2a532adbca18e2d22441ba417c6ac48741
PeterJackNaylor <peter.jack.naylor@gmail.com>
2022-11-14 10:42:20 +0900

    modified regroup to take into account OT and chunks

--- python/src/regroup.py
+++ python/src/regroup.py
@@ -15,16 +15,36 @@ dic = {
 
 for f in files:
     npz = np.load(f)
-    method, name = f.split("_")[:2]
-    if "chunks" in f:
-        import pdb; pdb.set_trace()
+    OT = "OT" in f
+    if OT:
+        name = f.split("_")[0]
+        method = "OT"
+        IoU_str_name = "IoU"
+    else:
+        method, name = f.split("_")[:2]
+        IoU_str_name = "IoU_bin"
+
+
+    if "Chunks" in f:
+        name = name[6:]
+        for i in range(4):
+            try:
+                chunks = int(name[:i])
+            except:
+                break
+        dataname = name[i:]
     else:
         if "tmp" in name:
-            name = name[3:]
+            dataname = name[3:]
         chunks = 0
+    
+    
+    IoU_bin = float(npz["IoU_str_name"])
 
-    IoU_bin = float(npz["IoU_bin"])
-    IoU_mc = float(npz["IoU_mc"])
+    if OT:
+        IoU_mc = 0.0
+    else:
+        IoU_mc = float(npz["IoU_mc"])
 
 
     dic["chunks"].append(chunks)
